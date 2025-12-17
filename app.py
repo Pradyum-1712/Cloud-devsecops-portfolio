5383ab648aef7d36fc04ccbaf84567c4cb274838
Pradyumvsamal@gmail.com <pradyumsamal@Pradyums-MacBook-Air.local>
2025-12-17 00:42:53 -0500

    Initial commit: Infrastructure and App

---
+++ app.py
@@ -0,0 +1,12 @@
+# app/app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello! This is my secure DevSecOps application."
+
+if __name__ == '__main__':
+    # Running on 0.0.0.0 makes it accessible outside the container
+    app.run(host='0.0.0.0', port=5000)
